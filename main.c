44552d2a0db85c13abf1d6b7ab4a55b449e8c983
Mariano Rodríguez <contacto@marianord.com>
2017-02-26 00:22:04 -0400

    Falta: Quitar salida por consola y pasarla a archivos, Eliminar archivos Core Dump, Reportes de los hijos en /tmp, Directorios sin permisos de lectura/ejecucion, Permiso de Escritura en Directorio Padre, Manejador de señales del hijo, Eliminar errorno

--- main.c
+++ main.c
@@ -1,26 +1,21 @@
-// Directivas
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h> // Manejo de señales
-#include <dirent.h> // Operaciones con directorios
-#include <sys/types.h>
-  
- // Librerías propias
-
-
-// Declaración de funciones
+// Librerías
+#include "archivos.h"
+#include "directorios.h"
 
 // Constantes y Variables Globales
-#define NAME_MAX 255    /* # chars in a file name */
-#define PATH_MAX 4096    /* # chars in a path name including nul */
-#define READ 0  // Pipe read
-#define WRITE 1 // Pipe write
 
 
+// Declaración de variables
+char raiz[PATH_MAX];
+struct dirent *fd;
+struct stat buffer;
+int pipeInfo[2];
+int pidHijo;
 
 // Main
 
-int main(int argc, char* argv[]) {
+/*int main(int argc, char* argv[]) {*/
+int main(void) {
 /*
   // Asegurarse de que hayan ingresado el nombre del archivo de salida
   if (argc < 2) {
@@ -32,18 +27,89 @@ int main(int argc, char* argv[]) {
   }
 */
   // Obtiene el directorio donde se está trabajando
-  char cwd[1024];
-   if (getcwd(cwd, sizeof(cwd)) != NULL)
-       fprintf(stdout, "Current working dir: %s\n", cwd);
-   else
-       perror("getcwd() error");
-   return 0;
-/*
+  char *raiz = obtenerDirectorioRaiz();
+
   // Abre el directorio
-  DIR *directorio = opendir();
-  opendir()
+  DIR *directorio = opendir(raiz);
+  Directorio *directorioStruct;
+  directorioStruct = datosDirectorioActual(raiz);
+  imprimeDirectorio("", *directorioStruct);
+
+  // Ciclo del proceso
+  while ((fd = readdir(directorio)) != NULL) {
+
+    stat(fd->d_name, &buffer);
+    
+
+    // Revisa que no sea un directorio oculto
+    if (fd->d_name[0] == '.') {
+      continue;
+    }
+    if (S_ISDIR(buffer.st_mode)) {
+
+      // Abre el pipe (recibe ambas puntas cerradas)
+      crearPipe(pipeInfo);
+
+      // Crea un proceso hijo
+      pidHijo = fork();
+      if(pidHijo == -1) { // Error
+        printf("fork() error.\n");
+        exit(-1);
+      } else if (pidHijo == 0) { // Es hijo
+        // Cierra la lectura
+        close(pipeInfo[READ]);
+        // Busca toda la información
+        Directorio *hijo;
+        hijo = datosDirectorioRecursivo(fd->d_name);
+        // Se la comunica al padre
+        //write(pipeInfo[WRITE], &hijo, sizeof(hijo));
+        write(pipeInfo[WRITE], hijo->path, sizeof(hijo->path));
+        write(pipeInfo[WRITE], &(hijo->cantArchivos), sizeof(hijo->cantArchivos));
+        write(pipeInfo[WRITE], &(hijo->informacion), sizeof(hijo->informacion));
+        write(pipeInfo[WRITE], &(hijo->grupo), sizeof(hijo->grupo));
+        write(pipeInfo[WRITE], &(hijo->usuario), sizeof(hijo->usuario));
+        write(pipeInfo[WRITE], &(hijo->fechaMod), sizeof(hijo->fechaMod));
+        write(pipeInfo[WRITE], &(hijo->fechaAcc), sizeof(hijo->fechaAcc));
+        write(pipeInfo[WRITE], &(hijo->bytes), sizeof(hijo->bytes));
+        // El hijo termina
+        free(hijo);
+        exit(0);
+      } else { // Es padre
+        // Cierra la escritura
+        close(pipeInfo[WRITE]);
+        // Espera al hijo
+        wait(0);
+        // Lee la información del pipe
+        Directorio hijo;
+        // Empiezo a leer el pipe
+        read(pipeInfo[READ], hijo.path, sizeof(hijo.path));
+        read(pipeInfo[READ], &(hijo.cantArchivos), sizeof(hijo.cantArchivos));
+        read(pipeInfo[READ], &(hijo.informacion), sizeof(hijo.informacion));
+        read(pipeInfo[READ], &(hijo.grupo), sizeof(hijo.grupo));
+        read(pipeInfo[READ], &(hijo.usuario), sizeof(hijo.usuario));
+        read(pipeInfo[READ], &(hijo.fechaMod), sizeof(hijo.fechaMod));
+        read(pipeInfo[READ], &(hijo.fechaAcc), sizeof(hijo.fechaAcc));
+        read(pipeInfo[READ], &(hijo.bytes), sizeof(hijo.bytes));
+        // Cierra el pipe
+        close(pipeInfo[READ]);
+
+        // Imprime la información del hijo
+        imprimeDirectorio(raiz, hijo);
+      }
+    
+    }
   }
-*/
+
+  // Cierra el directorio
+  closedir(directorio);
+
+  // Manejador de Señales
+  signal(SIGINT, manejadorDeSenales);
+  printf("\nTerminé\n");
+  while(1) {
+
+  };
+
   // Finaliza
   return 0;
 
--- makefile
+++ makefile
@@ -3,7 +3,7 @@
 # Variables
 CC = gcc
 CFLAGS = -g -Wall -Wextra
-OBJS = 
+OBJS = main.o directorios.o archivos.o librerias.o
 
 # Regla all
 all: els
@@ -14,7 +14,17 @@ els: $(OBJS)
 	gcc $(CFLAGS) -o els $(OBJS)
 
 # Creación de los Object-Files (.o)
+main.o: main.c archivos.o directorios.o librerias.o
+	gcc $(CFLAGS) -c main.c
 
+archivos.o: archivos.h librerias.o
+	gcc $(CFLAGS) -c archivos.c
+
+directorios.o: directorios.h librerias.o
+	gcc $(CFLAGS) -c directorios.c
+
+librerias.o: librerias.h
+	gcc $(CFLAGS) -c librerias.c
 
 # Clean
 
